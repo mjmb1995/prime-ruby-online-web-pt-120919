47bcaeae2b906f773f2d962ffb02e54f906c2939
Mark Bihag <markbihag330@gmail.com>
2020-01-03 04:39:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
 # Add  code here!
 def prime?(num)
+  prime = true
   if num < 0
     num = num * -1
   end
+  if num === 0 || num === 1
+    prime = false
+  end
   i = 2
-  prime = true
+
   while i <= num - 1
     if num % i === 0 && i != num
        prime = false
