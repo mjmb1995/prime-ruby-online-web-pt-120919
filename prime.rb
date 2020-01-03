0ddac233a1d68b0fa5ebc8d9c02c00cf9d3d6639
Mark Bihag <markbihag330@gmail.com>
2020-01-03 04:22:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(num)
   i = 0
-  [2..num].each do |num|
-    return true if int % num === 0
+  [2..num].each do |item|
+    return true if num % item === 0
     i += 1
   end
 end
