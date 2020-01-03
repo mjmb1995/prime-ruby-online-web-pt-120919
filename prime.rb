00e550d68e93c14bbce4f4605c29b6cad6d8d4b7
Mark Bihag <markbihag330@gmail.com>
2020-01-03 04:40:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  prime = true
+  i = 2
+  if num < 0
+    num = num * -1
+  end
+  if num === 0 || num === 1
+    prime = false
+  end
+  while i <= num - 1
+    if num % i === 0 && i != num
+       prime = false
+    end
+    i += 1
+  end
+  prime
+end
